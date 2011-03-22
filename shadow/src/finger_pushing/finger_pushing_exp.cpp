05e3df36c6268bf1d2577322de592a595495476f
Alexander Skoglund <alexander.skoglund@gmail.com>
2011-03-22 10:08:29 +0100

    Added msg/ShadowTargets.msg which was missing

--- shadow/src/finger_pushing/finger_pushing_exp.cpp
+++ shadow/src/finger_pushing/finger_pushing_exp.cpp
@@ -38,6 +38,9 @@
 #include <cstdlib>
 #include <vector>
 #include <ros/ros.h>
+#include <fstream>
+#include <iostream>
+#include <sys/time.h>
 //#include <boost/thread/mutex.hpp>
 
 #include "finger_pushing_class.h"
@@ -58,6 +61,7 @@ int main(int argc, char **argv)
   int i,j;
   double baseline_force=0;
   double force_max = 0;
+  double force_push_max = 0;
   int number_of_trials = 4;
   std::vector<unsigned int> test_case_code;
   int type;
@@ -72,16 +76,28 @@ int main(int argc, char **argv)
 
   // Create the object
   FingerPushing finger_pushing;
+  std::ofstream data_file; // Logfile
 
   // Init Shadow contoller and PAM
   finger_pushing.init();
   finger_pushing.empty_pam();
+
+  // Open data file for printing
+  time_t t = time(0);
+  struct tm* lt = localtime(&t);
+  char time_str[256];
+  sprintf(time_str, "../data/datalog_%04d%02d%02d_%02d%02d%02d.txt",
+          lt->tm_year + 1900, lt->tm_mon + 1, lt->tm_mday,
+          lt->tm_hour, lt->tm_min, lt->tm_sec);
+ 
+  data_file.open(time_str);
   
   // Check for parameters on parameter server
   if (finger_pushing.nh_.hasParam("/shadow/max_pull_force"))
     {
       finger_pushing.nh_.getParam("/shadow/max_pull_force", force_max); 
       ROS_INFO("/shadow/max_pull_force is %f", force_max );
+      data_file << "/shadow/max_pull_force is " << force_max << std::endl;
     }
   else
     {
@@ -94,6 +110,7 @@ int main(int argc, char **argv)
     {
       finger_pushing.nh_.getParam("/shadow/baseline_force", baseline_force); 
       ROS_INFO("/shadow/baseline_force %f", baseline_force );
+      data_file << "/shadow/baseline_force " << baseline_force << std::endl;
     }
   else
     {
@@ -101,13 +118,25 @@ int main(int argc, char **argv)
       ROS_ERROR("Run the finger_pushing_ini program!");
       exit(0);
     }
- 
+  if (finger_pushing.nh_.hasParam("/shadow/max_push_force"))
+    {
+      finger_pushing.nh_.getParam("/shadow/max_push_force", force_push_max); 
+      ROS_INFO("/shadow/max_push_force is %f", force_push_max );
+      data_file << "/shadow/max_push_force is " << force_push_max << std::endl;
+    }
+  else
+    {
+      ROS_WARN("/shadow/max_pull_force does not exits!");
+      ROS_WARN("Run the finger_pushing_ini program!");
+    }
 
   if (finger_pushing.nh_.hasParam("/shadow/number_of_trials"))
     finger_pushing.nh_.getParam("/shadow/number_of_trials", number_of_trials); 
   else
     ROS_INFO("Number of trials is not set on the parameter server, using default = %d",number_of_trials);    
 
+  data_file << "------------------------------------" << std::endl;
+  
   // Generate the vector with test cases in radom order
   for(i =0; i < number_of_trials;i++)
     {
@@ -130,16 +159,19 @@ int main(int argc, char **argv)
 	{
 	case 0:
 	  ROS_INFO("Isometric force, scale %f\nFixate the screw and push the button to start",scale);
+          data_file << i << "\t"<< "Isometric force, scale " << scale << std::endl;
 	  finger_pushing.wait_button_push(PUSH_BUTTON, 0.0);
 	  finger_pushing.record_max_sensor_data_time(0,exp_duration);
 	  break;
 	case 1:
 	  ROS_INFO("Dynamic force, scale %f\nLoosen the screw and push the button to start",scale);
+          data_file << i << "\t"<< "Dynamic force, scale " << scale << std::endl;
 	  finger_pushing.wait_button_push(PUSH_BUTTON, 0.0);
 	  finger_pushing.record_max_sensor_data_time(0,exp_duration);
 	  break;
 	case 2:
 	  ROS_INFO("Loosen the screw!\nActive force will now be produced at %.1f%c",100*scale, 0x25);
+          data_file << i << "\t"<< "Active force, scale " << scale << std::endl;
 	  ROS_INFO("Push the button to active PAM");
 	  finger_pushing.wait_button_push(PUSH_BUTTON, 0.0);
 	 
@@ -167,4 +199,9 @@ int main(int argc, char **argv)
       if( !finger_pushing.measure_baseline(FLEXIFORCE_AIRMUSCLE,baseline_force) )
 	ROS_WARN("Baseline seems to have changed");
     }
+    data_file.close();
+
 }
+
+
+
