a1c74d9eaf308d3d132fd51465910e52a252a6ee
Alexander Skoglund <alexander.skoglund@gmail.com>
2011-08-25 13:42:50 +0200

    Changed frequency for templog file.

--- labjack/src/lj_temp/lj_temp.cpp
+++ labjack/src/lj_temp/lj_temp.cpp
@@ -59,7 +59,7 @@ using namespace ros;
                                     //1-25 for 1 StreamData response per packet.
 boost::mutex g_labjack_mutex;
 
-LabjackTemp::LabjackTemp(): private_nh_("~"), publish_rate_(1), publish_duration_(0.200)  //init variabels 
+LabjackTemp::LabjackTemp(): private_nh_("~"), publish_rate_(1), publish_duration_(1.000)  //init variabels 
 {
    // 5 Hz
   resolutionIndex_ = 1;
